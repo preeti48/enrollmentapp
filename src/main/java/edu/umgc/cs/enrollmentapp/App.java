a6de219422a4fffc56e12dce2cc2012227bcf593
Preethi Suresh <preethisuresh@Preethis-MBP.T-mobile.com>
2019-11-07 13:06:32 -0500

    First commit

---
+++ src/main/java/edu/umgc/cs/enrollmentapp/App.java
@@ -0,0 +1,37 @@
+package edu.umgc.cs.enrollmentapp;
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+        
+    }
+}
