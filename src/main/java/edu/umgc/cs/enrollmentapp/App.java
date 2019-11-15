c7da3e7be09afcecd0e5733c838214ca8dec6c36
ochandran <57119908+ochandran@users.noreply.github.com>
2019-11-15 16:54:32 -0500

    Update App.java

--- src/main/java/edu/umgc/cs/enrollmentapp/App.java
+++ src/main/java/edu/umgc/cs/enrollmentapp/App.java
@@ -1,38 +1,39 @@
 package edu.umgc.cs.enrollmentapp;
+
 import javax.swing.*;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-	
-	
-	private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-	frame.setBounds(100,200,450,300);
-    }
-	
-    public static void main( String[] args )
+public class App {
+
+//	private static void createAndShowGUI() {
+//        //Create and set up the window.
+//        JFrame frame = new JFrame("HelloWorldSwing");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+// 
+//        //Add the ubiquitous "Hello World" label.
+//        JLabel label = new JLabel("Hello World");
+//        frame.getContentPane().add(label);
+// 
+//        //Display the window.
+//        frame.pack();
+//        frame.setVisible(true);
+//	frame.setBounds(100,200,450,300);
+//    }
+	TabGui tabGui = new TabGui();
+
+	public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-        
+        TabGui tabGui = new TabGui();
+//        
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+        //createAndShowGUI();
+//            	
+//        });
+//        
     }
 }
