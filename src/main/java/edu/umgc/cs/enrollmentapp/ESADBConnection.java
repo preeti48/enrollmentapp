c52663341fdfd6be5e2a4e68b47b71461b8a0055
nngyen <57246707+nngyen@users.noreply.github.com>
2019-11-20 16:10:26 -0500

    Update ESADBConnection.java

--- src/main/java/edu/umgc/cs/enrollmentapp/ESADBConnection.java
+++ src/main/java/edu/umgc/cs/enrollmentapp/ESADBConnection.java
@@ -9,7 +9,9 @@ public class ESADBConnection {
 	{
 		try {
 			Class.forName("org.sqlite.JDBC");
+			// will need to path your own database location **this is example from me** 
 			Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Senpai\\eclipse-workspace\\ESA.db");
+			//Display Successful Connection Message Dialog
 			JOptionPane.showMessageDialog(null, "ESA DataBase Connected Successfully");
 			return conn;
 		}
